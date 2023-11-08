907926a258a88e98be9281c56569da6aa8cf55b3
Rick Kauffman <rick@rickkauffman.com>
2023-11-08 18:08:19 -0500

    init

---
+++ portgroup.py
@@ -0,0 +1,109 @@
+'''
+888888888888                                      88              88
+         ,88                                      88              88
+       ,88"                                       88              88
+     ,88"     ,adPPYba,  8b,dPPYba,   ,adPPYba,   88  ,adPPYYba,  88,dPPYba,
+   ,88"      a8P_____88  88P'   "Y8  a8"     "8a  88  ""     `Y8  88P'    "8a
+ ,88"        8PP"""""""  88          8b       d8  88  ,adPPPPP88  88       d8
+88"          "8b,   ,aa  88          "8a,   ,a8"  88  88,    ,88  88b,   ,a8"
+888888888888  `"Ybbd8"'  88           `"YbbdP"'   88  `"8bbdP"Y8  8Y"Ybbd8"'
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0.
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
+__author__ = "@netwookie"
+__credits__ = ["Rick Kauffman"]
+__license__ = "Apache2"
+__version__ = "0.1.1"
+__maintainer__ = "Rick Kauffman"
+__status__ = "Alpha"
+
+
+Usage: This python file reverts and resets all VM's and deletes all Distributed
+Virtual switches
+
+'''
+from pyVmomi import vim
+from pyVim.task import WaitForTask
+from pyVim.connect import SmartConnect, Disconnect
+import ssl
+import datetime
+import logging
+import time
+
+
+def makepgrp(dv_port_name,vlan_number,dvs_name,vsphere_ip,vshpere_user,vsphere_pass):
+
+    sslContext = ssl._create_unverified_context()
+
+    port="443"
+
+    # Create a connector to vsphere
+    si = SmartConnect(
+        host=vsphere_ip,
+        user=vsphere_user,
+        pwd=vsphere_pass,
+        port=port,
+        sslContext=sslContext
+    )
+
+    content = si.RetrieveContent()
+
+    dvs_list = content.viewManager.CreateContainerView(content.rootFolder,
+                                                     [vim.DistributedVirtualSwitch],
+                                                     True)
+        for dv_switch in dvs_list.view:
+            # Look through the list of DVS and find the match
+            if dv_switch.name == dvs_name:
+                response = add_dvPort_group(si, dv_switch, dv_port_name, vlan_number)
+
+    return response
+
+def wait_for_task(task, actionName='job', hideResult=False):
+
+    while task.info.state == vim.TaskInfo.State.running:
+        time.sleep(5)
+
+    if task.info.state == vim.TaskInfo.State.success:
+        out = '%s Port group added to DVS.' % actionName
+        logging.warning(out)
+    else:
+        out = '%s Port group is running long...no worries: %s' % (actionName, task.info.state)
+        logging.warning(out)
+
+    return task.info.result
+
+def add_dvPort_group(si, dv_switch, dv_port_name, vlan_number):
+
+    dv_pg_spec = vim.dvs.DistributedVirtualPortgroup.ConfigSpec()
+    dv_pg_spec.name = dv_port_name
+    dv_pg_spec.numPorts = 8
+    dv_pg_spec.type = vim.dvs.DistributedVirtualPortgroup.PortgroupType.earlyBinding
+    dv_pg_spec.defaultPortConfig = vim.dvs.VmwareDistributedVirtualSwitch.VmwarePortConfigPolicy()
+    dv_pg_spec.defaultPortConfig.securityPolicy = vim.dvs.VmwareDistributedVirtualSwitch.SecurityPolicy()
+
+    vlan_spec = vim.dvs.VmwareDistributedVirtualSwitch.VlanIdSpec()
+    vlan_spec.vlanId = vlan_number
+    dv_pg_spec.defaultPortConfig.vlan = vlan_spec
+
+    dv_pg_spec.defaultPortConfig.securityPolicy.allowPromiscuous = vim.BoolPolicy(value=True)
+    dv_pg_spec.defaultPortConfig.securityPolicy.forgedTransmits = vim.BoolPolicy(value=True)
+
+    dv_pg_spec.defaultPortConfig.vlan.inherited = False
+    dv_pg_spec.defaultPortConfig.securityPolicy.macChanges = vim.BoolPolicy(value=False)
+    dv_pg_spec.defaultPortConfig.securityPolicy.inherited = False
+    task = dv_switch.AddDVPortgroup_Task([dv_pg_spec])
+
+    wait_for_task(task)
+
+    return task.info.result
